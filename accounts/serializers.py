dba61f7d2d19f153658d43dc48c01b52ef77b8ec
Evgeniy K <pgg1337@mail.ru>
2023-11-24 11:17:56 +0200

    add user registration functional

---
+++ accounts/serializers.py
@@ -0,0 +1,10 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+user_model = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = user_model
+        fields = ['first_name', 'username', 'email', 'password']
--- accounts/views.py
+++ accounts/views.py
@@ -1,2 +1,25 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics, status
+from rest_framework.response import Response
 
+from .serializers import UserSerializer
+
+user_model = get_user_model()
+
+
+class RegistrationUserAPIView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    queryset = user_model.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            user_model.objects.create_user(
+                username=serializer.data['username'],
+                password=serializer.data['password'],
+                first_name=serializer.data['first_name'],
+                email=serializer.data['email']
+            )
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
